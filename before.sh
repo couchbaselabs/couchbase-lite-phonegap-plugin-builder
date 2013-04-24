42e08ad3c24340a15c820de5cc997bfd1d5f0472
Chris Anderson <jchris@couchbase.com>
2013-04-24 16:39:52 -0700

    sorta worked

--- before.sh
+++ before.sh
@@ -1,5 +1,9 @@
+echo "run this once"
+npm install
 cd vendor
 # test for CouchbaseLite*.zip
 curl -O https://dl.dropboxusercontent.com/u/360302/CouchbaseLite-a1c1.zip
 open CouchbaseLite*.zip
+# download JavaScript core
+
 node before.js
--- vendor/before.js
+++ vendor/before.js
@@ -3,25 +3,29 @@ var builder = require("xmlbuilder"),
 	fs = require('fs'),
 	find = require("findit").find;
 
-var ios = builder.create("plugin",{
-	'xmlns':"http://www.phonegap.com/ns/plugins/1.0",
-    'xmlns:android':"http://schemas.android.com/apk/res/android",
-    'id':"com.couchbase.litegap",
-    'version':"0.1.0"
-})
-.ele("name", "LiteGap").up()
-.ele("asset",{src:"www/couchbase-lite.js"}).up()
+var xml = builder.create("plugin");
+
+xml.att('xmlns',"http://www.phonegap.com/ns/plugins/1.0")
+xml.att('xmlns:android',"http://schemas.android.com/apk/res/android")
+xml.att('id',"com.couchbase.litegap")
+xml.att('version',"0.1.0")
+
+
+var ios = xml.ele("name", "LiteGap").up()
+.ele("asset",{src:"www/litegap.js"}).up()
 .ele("platform", {name:"ios"})
 	.ele("config-file", {target:"config.xml", parent:"/widget/plugins"})
 		.ele("plugin", {name:"LiteGap", value : "LiteGap"}).up().up();
 
+ios.ele("header-file",{src:"LiteGap.h"});
+ios.ele("source-file",{src:"LiteGap.m"});
 
 var finder = find("CouchbaseLite-a1c1/iOS");
 finder.on("file", function(file) {
 	if (/.*\.h/.test(file)) {
-		ios.ele("header-file",{src:"../../vendor/"+file});
+		ios.ele("framework",{src:"../../vendor/"+file});
 	} else {
-		ios.ele("resource-file",{src:"../../vendor/"+file});
+		ios.ele("framework",{src:"../../vendor/"+file});
 	}
 })
 
